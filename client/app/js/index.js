a68a1856db52afa871cde74159a7dd1a415f04a7
Jake Ritter <jakeritter77@gmail.com>
2022-05-15 13:19:13 -0700

    - added db - testing soft keyboard issue

--- client/app/js/index.js
+++ client/app/js/index.js
@@ -4,6 +4,16 @@ import { getBills } from "./requests.js";
 
 // Initialize navigation
 
+setTimeout(() => {
+  let viewHeight = window.visualViewport.height;
+  let viewWidth = window.visualViewport.width;
+  let viewport = document.querySelector("meta[name=viewport");
+  viewport.setAttribute(
+    "content",
+    `height=${viewHeight}, width=${viewWidth}, initial-scale=1.0`
+  );
+}, 300);
+
 async function startApp() {
   await getBills();
 
